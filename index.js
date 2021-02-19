6b5e9e3e9f7d473f2d8a276ffc52ee8e3dab9343
Fabio Vedovelli <vedovelli@gmail.com>
2021-02-19 10:38:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 console.log(`
 Fabio Vedovelli
 
-Curso Testes JS - https://javascript.tv.br
-Mini Cursos     - https://classes.vedovelli.com.br
-Youtube         - https://www.youtube.com/vedovelli
+Curso Testes JS   - https://javascript.tv.br
+Mini Cursos       - https://classes.vedovelli.com.br
+Youtube           - https://www.youtube.com/vedovelli
 
-Website         - https://vedovelli.com.br
+Website           - https://vedovelli.com.br
 
-LinkedIn        - https://linkedin.com/in/vedovelli
-Twitter         - https://www.twitter.com/vedovelli74
-Instagram       - https://instagram.com/vedovellifabio
-Github          - https://www.github.com/vedovelli
+LinkedIn          - https://linkedin.com/in/vedovelli
+Twitter           - https://www.twitter.com/vedovelli74
+Instagram         - https://instagram.com/vedovellifabio
+Github            - https://www.github.com/vedovelli
 `);
